b46010d970a22d5ee357d7e430c7924ec7306da5
Saif <saifmkholy@gmail.com>
2025-12-05 01:55:46 +0100

    LATEST

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\ReviewResource;
 use App\Services\ReviewService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ReviewController extends Controller
 {
