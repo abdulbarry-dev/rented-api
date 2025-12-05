595a8516560f04603499b9cb85433924d4162d87
Saif <saifmkholy@gmail.com>
2025-12-05 15:26:25 +0100

    fix: Use Storage::url() for avatar URLs to respect APP_URL and port configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -66,7 +67,7 @@ class User extends Authenticatable
             return null;
         }
 
-        return asset('storage/'.$this->avatar_path);
+        return Storage::disk('public')->url($this->avatar_path);
     }
 
     /**
